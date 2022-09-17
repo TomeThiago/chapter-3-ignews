de916e0409502b39dbd2c53bad0cfe3f1e61464e
Thiago Tomé <thiago.tome@destrava.ai>
2022-09-17 15:07:08 -0300

    chapter 3 finished

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,32 @@
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from "next-auth/react"
+
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+  const { data } = useSession();
+
+  const isUserLoggedIn = !!data;
+
+  return isUserLoggedIn ? (
+    <button 
+      className={styles.signButton}
+      type="button"
+      onClick={() => signOut()}
+    >
+      <FaGithub color="#04d361" />
+      {data.user.name}
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button
+      className={styles.signButton}
+      type="button"
+      onClick={() => signIn('github')}
+    >
+      <FaGithub color="#eba417" />
+      Sign In with GitHub
+    </button>
+  )
+}
---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,52 @@
+import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+import styles from './styles.module.scss';
+
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data } = useSession();
+  const router = useRouter();
+
+  async function handleSubscribe() {
+    if (!data.user) {
+      signIn('github');
+
+      return;
+    }
+
+    if(data.activeSubscription) {
+      router.push('/posts');
+
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe', {
+        priceId,
+      });
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+
+  return (
+    <button
+      className={styles.subscribeButton}
+      type="button"
+      onClick={handleSubscribe}
+    >
+      Subscribe Now
+    </button>
+  )
+}
